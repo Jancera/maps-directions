fe34968f9e2cc0ab43012281c0efbe8ad264f725
Jancer Lima <contato.jancersl@gmail.com>
2021-11-08 00:29:16 -0300

    Aula 23.4

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-import MapView, { Marker, Callout } from "react-native-maps";
+import MapView, { Marker, Callout, Polygon } from "react-native-maps";
 
 import * as Location from "expo-location";
 
@@ -27,6 +27,42 @@ const App = () => {
     setMarker([...marker, coordinate]);
   };
 
+  const poly = [
+    {
+      latitude: 37.440996809186295,
+      longitude: -122.10251212120055,
+    },
+    {
+      latitude: 37.44424786944107,
+      longitude: -122.06525556743145,
+    },
+    {
+      latitude: 37.406559059108694,
+      longitude: -122.0656116306781911,
+    },
+    {
+      latitude: 37.4113168031456,
+      longitude: -122.10595373064278,
+    },
+  ];
+
+  const holes = [
+    [
+      {
+        latitude: 37.431058810801524,
+        longitude: -122.09361355751751,
+      },
+      {
+        latitude: 37.43171774653013,
+        longitude: -122.07801215350628,
+      },
+      {
+        latitude: 37.41810362212148,
+        longitude: -122.0850120484829,
+      },
+    ],
+  ];
+
   return (
     <View style={styles.container}>
       <MapView
@@ -42,7 +78,7 @@ const App = () => {
         loadingEnabled
         mapType="terrain"
       >
-        {marker.length > 0 &&
+        {/*    {marker.length > 0 &&
           marker.map((m) => {
             return (
               <Marker
@@ -56,7 +92,14 @@ const App = () => {
                 </Callout>
               </Marker>
             );
-          })}
+          })} */}
+        <Polygon
+          coordinates={poly}
+          strokeWidth={5}
+          strokeColor="red"
+          fillColor="rgba(255,255,0,.2)"
+          holes={holes}
+        />
       </MapView>
     </View>
   );
